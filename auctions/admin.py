f890ccd54d1cd59d14acfe62aba68db429b008a1
Dhita Irma <dhita.irma@gmail.com>
2020-08-28 22:32:28 +0800

    Update Admin.py: add list_display

--- auctions/admin.py
+++ auctions/admin.py
@@ -2,7 +2,18 @@ from django.contrib import admin
 
 from .models import *
 
-# Register your models here.
-admin.site.register(User)
-admin.site.register(Listing)
+
+class UserAdmin(admin.ModelAdmin):
+    list_display = ("id",)
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "seller")
+
+class BidAdmin(admin.ModelAdmin):
+    list_display = ("id", "item", "bidder", "price") 
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Listing, ListingAdmin)
 admin.site.register(Category)
+admin.site.register(Bid, BidAdmin)
